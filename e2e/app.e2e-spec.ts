565780449f24d27cb10cc1be82666e5c0435ee7b
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2018-03-24 14:31:55 +0530

    End-to-End Test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,14 @@ describe('con-fusion App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+    let navlink = page.getAllElements('a').get(1);
+    navlink.click();
+    expect(page.getParagraphText('h3')).toBe('About Us');
   });
 });
